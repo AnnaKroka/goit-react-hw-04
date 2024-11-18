8eeb05427418030d32a5956785849bcc5c864d3d
Anna Kroka <avkroka.broker@gmail.com>
2024-11-18 00:26:18 -0800

    practice working with asios

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,37 @@
 import { useEffect, useState } from 'react'
 import s from "./App.module.css"
+import axios from 'axios';
+import ArticleList from './ArticleList/ArticleList';
 
-import ContactForm from './ContactForm/ContactForm'
-import SearchBox from './SearchBox/SearchBox'
-import ContactList from './ContactList/ContactList'
 
 function App() {
  
-  const [contacts, setContacts] = useState(() => 
-    JSON.parse(localStorage.getItem('contacts')) ??
-    [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ]);
+const [articles, setArticles] = useState([]);
 
-  const [filter, setFilter] = useState('');
-
-  const visibleContacts = contacts.filter((contact) =>
-  contact.name.toLowerCase().includes(filter.toLowerCase()));
-
-  const addContact = (newContact) => {
-    setContacts((prevContacts) => {
-      return [...prevContacts, newContact];
-    })
-  };
-
-  const deleteContact = (contactId) => {
-    setContacts((prevContacts) => {
-      return prevContacts.filter((task) => task.id !== contactId);
-  })
-  };
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
+useEffect(() => {
+async function fetchArticles() {
+  // Тут виконуємо HTTP-запит
+  const response = await axios.get(
+    "https://hn.algolia.com/api/v1/search?query=react"
+  );
+  // 2. Записуємо дані HTTP-запитy в стан
+  setArticles(response.data.hits);
+}
+fetchArticles();
+}, []);
 
   return (
     <>
     
       <div className={s.container}>
-      <h1>Phonebook</h1>
-      <ContactForm onAdd={addContact}/>
-      <SearchBox value={filter} onFilter={setFilter} />
-      <ContactList contacts={visibleContacts} onDelete={deleteContact} />
+      <h1>Latest articles</h1>
+      {articles.length > 0 &&
+        <ArticleList items={articles} />
+      }
       </div>
 
     </>
-  )
-}
+  );
+};
 
 export default App
